02831f21f719398a366edf8e1ee2d8b9600858c0
Nitin Kumar <nikk7315@gmail.com>
2024-03-14 14:48:56 +0530

    finall

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,13 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req,res)=>{
+  res.json({
+    success:true,
+    message:"WELCOME",
+  })
+})
+
 app.use("/api/v1/message", messageRouter);
 
 dbConnection();
